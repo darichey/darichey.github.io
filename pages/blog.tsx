5a2e8b3bf48570e3668199ae70c48769e155da0e
David Richey <darichey1@gmail.com>
2022-04-06 23:40:10 -0500

    Use next-isms (like Link and Image)

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,44 +1,63 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import Link from "next/link";
+import Image from "next/image";
 
 const Blog: NextPage = () => {
   return (
     <div className="main">
-        <section className="nav">
-            <a href="/">Home</a>
-            <a href="/blog">Blog</a>
-            <a href="https://github.com/darichey">GitHub</a>
-        </section>
+      <section className="nav">
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/blog">
+          <a>Blog</a>
+        </Link>
+        <Link href="https://github.com/darichey">
+          <a>GitHub</a>
+        </Link>
+      </section>
 
-        <section className="header">
-            <img className="pic" src="/img/david.png" />
-            <div className="titles">
-                <h1 className="title">David's Blog</h1>
-                <h3 className="subtitle">Sometimes I write stuff.</h3>
-            </div>
-        </section>
+      <section className="header">
+        <div className="pic">
+          <Image
+            src="/img/david.png"
+            alt="Picture of me"
+            layout="responsive"
+            width={1}
+            height={1}
+          />
+        </div>
+        <div className="titles">
+          <h1 className="title">David&apos;s Blog</h1>
+          <h3 className="subtitle">Sometimes I write stuff.</h3>
+        </div>
+      </section>
 
-        <section className="content">
-            <p>
-                From time to time, like to write about things that I'm learning - or worse, my <i>opinions</i>.
-            </p>
-            <p>
-                Keep in mind that I am by no means an authoritative source on anything I write about.
-                I write these posts in order to aid my own learning and in the hope that others can learn from them.
-            </p>
+      <section className="content">
+        <p>
+          From time to time, like to write about things that I&apos;m learning -
+          or worse, my <i>opinions</i>.
+        </p>
+        <p>
+          Keep in mind that I am by no means an authoritative source on anything
+          I write about. I write these posts in order to aid my own learning and
+          in the hope that others can learn from them.
+        </p>
 
-            <p>
-                I'm currently working on getting asciidoctor working so these posts can look nice. Here's a preview of
-                what I plan on posting first &#128522;
-            </p>
-            <ul>
-                <li>Arity of Types</li>
-                <li>Trampolines in Java</li>
-                <li>String Concatenation in Java</li>
-                <li>Subtyping Variance</li>
-            </ul>
-        </section>
+        <p>
+          I&apos;m currently working on getting asciidoctor working so these
+          posts can look nice. Here&apos;s a preview of what I plan on posting
+          first &#128522;
+        </p>
+        <ul>
+          <li>Arity of Types</li>
+          <li>Trampolines in Java</li>
+          <li>String Concatenation in Java</li>
+          <li>Subtyping Variance</li>
+        </ul>
+      </section>
     </div>
   );
-}
+};
 
 export default Blog;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,94 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
     <div className="main">
-        <section className="nav">
-            <a href="/">Home</a>
-            <a href="/blog">Blog</a>
-            <a href="https://github.com/darichey">GitHub</a>
-        </section>
+      <section className="nav">
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/blog">
+          <a>Blog</a>
+        </Link>
+        <Link href="https://github.com/darichey">
+          <a>GitHub</a>
+        </Link>
+      </section>
 
-        <section className="header">
-            <img className="pic" src="/img/david.png" />
-            <div className="titles">
-                <h1 className="title">Hey, I'm David.</h1>
-                <h3 className="subtitle">Sometimes I code stuff.</h3>
-            </div>
-        </section>
+      <section className="header">
+        <div className="pic">
+          <Image
+            src="/img/david.png"
+            alt="Picture of me"
+            layout="responsive"
+            width={1}
+            height={1}
+          />
+        </div>
 
-        <section className="content">
-            <p>
-                I'm a senior at UT Dallas pursuing a degree in Computer Science and minoring in Mathematics.
-            </p>
+        <div className="titles">
+          <h1 className="title">Hey, I&apos;m David.</h1>
+          <h3 className="subtitle">Sometimes I code stuff.</h3>
+        </div>
+      </section>
 
-            <p>
-                I mostly program on the JVM with Java, Scala, and Kotlin. Recently, I've been learning some Haskell,
-                Rust, and TypeScript. I'm interested in functional programming, programming language design, type
-                theory, and category theory.
-            </p>
+      <section className="content">
+        <p>
+          I&apos;m a senior at UT Dallas pursuing a degree in Computer Science
+          and minoring in Mathematics.
+        </p>
 
-            <p>
-                I have a passion for teaching, and sometimes I like to write a bit about what I'm learning.
-            </p>
+        <p>
+          I mostly program on the JVM with Java, Scala, and Kotlin. Recently,
+          I&apos;ve been learning some Haskell, Rust, and TypeScript. I&apos;m
+          interested in functional programming, programming language design,
+          type theory, and category theory.
+        </p>
 
-            <p>
-                Online, I often go by Panda.
-            </p>
-        </section>
+        <p>
+          I have a passion for teaching, and sometimes I like to write a bit
+          about what I&apos;m learning.
+        </p>
 
-        <section className="links">
-            <a href="https://github.com/darichey"><img src="img/github_mark.svg" /></a>
-            <a href="https://www.linkedin.com/in/darichey/"><img src="img/linkedin_mark.svg" /></a>
-            <a href="mailto:darichey1@gmail.com"><img src="img/mail.svg" /></a>
-        </section>
+        <p>Online, I often go by Panda.</p>
+      </section>
+
+      <section className="links">
+        <Link href="https://github.com/darichey">
+          <a>
+            <Image
+              src="/img/github_mark.svg"
+              alt="Github Icon"
+              height={32}
+              width={32}
+            />
+          </a>
+        </Link>
+        <Link href="https://www.linkedin.com/in/darichey/">
+          <a>
+            <Image
+              src="/img/linkedin_mark.svg"
+              alt="LinkedIn Icon"
+              height={32}
+              width={32}
+            />
+          </a>
+        </Link>
+        <Link href="mailto:darichey1@gmail.com">
+          <a>
+            <Image
+              src="/img/mail.svg"
+              alt="Mail Icon"
+              height={32}
+              width={32}
+            />
+          </a>
+        </Link>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
