2377496cf4f20247cd85f70137d47c57df5d1463
David Richey <darichey1@gmail.com>
2023-02-08 23:13:47 -0600

    Adjust styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       </Head>
 
       <div className="flex justify-center my-10">
-        <div className="flex flex-col w-4/5 md:w-2/5 gap-4">
+        <div className="flex flex-col w-4/5 md:w-1/3 gap-4">
           <Nav />
           <Component {...pageProps} />
         </div>
