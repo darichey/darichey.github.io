b08a4f2bb8e7c009cf4a39eeb5eb07900c405b86
David Richey <darichey1@gmail.com>
2023-12-22 22:21:56 -0600

    Run prettier

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,14 +4,7 @@ const Card = ({ name }: { name: string }) => {
   const width = 100;
   const height = width * 1.4;
 
-  return (
-    <Image
-      src={`/img/cards/${name}`}
-      width={width}
-      height={height}
-      alt={name}
-    />
-  );
+  return <Image src={`/img/cards/${name}`} width={width} height={height} alt={name} />;
 };
 
 export default Card;
--- components/Cards.tsx
+++ components/Cards.tsx
@@ -9,7 +9,13 @@ const Cards = ({ cards, highlighted }: CardsProps) => {
   return (
     <div className="grid grid-cols-3 grid-rows-[repeat(6,_3em)_auto] place-items-start justify-items-center">
       {cards.map((card, idx) => (
-        <div key={card} style={{ zIndex: Math.floor(idx / 3), boxShadow: highlighted?.includes(card) ? "0 0 20px blue" : undefined }}>
+        <div
+          key={card}
+          style={{
+            zIndex: Math.floor(idx / 3),
+            boxShadow: highlighted?.includes(card) ? "0 0 20px blue" : undefined,
+          }}
+        >
           <Card name={card} />
         </div>
       ))}
