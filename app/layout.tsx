f5eb325d6407cee4ff7c5d0ea53e401b894d2eb6
David Richey <darichey1@gmail.com>
2023-12-27 16:41:13 -0600

    Add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <div className="flex flex-col max-w-[80ch] gap-12 justify-center mt-5 mb-20 p-[2ch] mx-auto">
+        <div className="flex flex-col max-w-[80ch] gap-12 justify-center mt-5 mb-5 p-[2ch] mx-auto">
           <Nav />
           <div>{children}</div>
+          <footer className="border-t-[1px] border-t-slate-900 p-2"></footer>
         </div>
       </body>
     </html>
