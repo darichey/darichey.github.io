1777407c924cbbe9c7e704f874a9e291e0bbb975
David Richey <darichey1@gmail.com>
2023-12-27 16:52:54 -0600

    Add threads link

--- components/Links.tsx
+++ components/Links.tsx
@@ -10,6 +10,9 @@ export default function Links() {
       <Link href="https://www.linkedin.com/in/darichey/">
         <Image src="/img/linkedin_mark.svg" alt="LinkedIn Icon" height={32} width={32} />
       </Link>
+      <Link href="https://www.threads.net/@davidrichey1">
+        <Image src="/img/threads_mark.svg" alt="Threads Icon" height={32} width={32} />
+      </Link>
       <Link href="mailto:darichey1@gmail.com">
         <Image src="/img/mail.svg" alt="Mail Icon" height={32} width={32} />
       </Link>
