847fdbc86338ced0cda53887fba09fd1c4043b6b
David Richey <darichey1@gmail.com>
2023-12-27 20:12:56 -0600

    Add slight background color to light code blocks

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Nav from "components/Nav";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-zinc-50 dark:bg-zinc-900">
+      <body className="dark:bg-zinc-900">
         <div className="flex flex-col max-w-[80ch] gap-12 justify-center mt-5 mb-5 p-[2ch] mx-auto">
           <Nav />
           {children}
